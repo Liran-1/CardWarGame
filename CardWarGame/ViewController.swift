9f44aba77b8be778202ccc820434d24b6ede3022
Liran Sorokin <Liran.Sorokin@s.afek.ac.il>
2024-07-07 20:19:40 +0300

    Initial Commit

---
+++ CardWarGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWarGame
+//
+//  Created by Student20 on 07/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
